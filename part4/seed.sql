e829a1766b8c9b0949e57b04ee7d87b258551c53
Ruwais1 <liruwais1@gmail.com>
2026-08-20 17:24:47 +0300

    Update Tasks and README.md

--- part4/seed.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO users (id, first_name, last_name, email, password, is_admin)
-VALUES ('36c08e80-ae9e-4512-a809-1162b24d9f16', 'Admin', 'HBnB', 'admin@hbnb.com', '$2b$12$QU199VJWbC7K/l85EsEPIuWKB/G2omQnt1B.M27jxWxiWaoJwKgiu', TRUE);
-
-INSERT INTO amenity (id, name) VALUES
-('a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11', 'WiFi'),
-('b1fead00-0d1c-5ff9-cc7e-7cc0ce491b22', 'Swimming Pool'),
-('c2ffbe11-1e2d-600a-dd8f-8dd1df502c33', 'Air Conditioning');
